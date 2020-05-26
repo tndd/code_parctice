db94c6b30a123671a2a6b30b1b923154506204c6
tanin <tanin@ikyu.com>
2020-05-26 21:40:23 +0900

    lcm_mltを廃止し、for文に変更

--- abc070c.py
+++ abc070c.py
@@ -8,15 +8,12 @@ def lcm(a, b):
     _gcd = gcd(a, b)
     return int((a * b) / _gcd)
 
-def lcm_mlt(a, num_list):
-    if len(num_list) == 0:
-        return a
-    n = num_list.pop()
-    a = max(a, n)
-    b = min(a, n)
-    return lcm_mlt(lcm(a, b), num_list)
-
 n = int(input())
-nl = [int(input()) for _ in range(n)]
 
-print(lcm_mlt(1, nl))
+ans = 1
+for i in range(n):
+    t = int(input())
+    a = max(ans, t)
+    b = min(ans, t)
+    ans = lcm(a, b)
+print(ans)
