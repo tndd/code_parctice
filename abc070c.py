54107a25c82351f1ebf220be523c5cc63ac57853
tanin <tanin@ikyu.com>
2020-05-26 21:37:18 +0900

    コメント削除と余分なset削除

--- abc070c.py
+++ abc070c.py
@@ -4,17 +4,10 @@ def gcd(a, b):
         return b
     return gcd(b, md)
 
-# print(gcd(1071, 1029))
-# 21
-
 def lcm(a, b):
     _gcd = gcd(a, b)
     return int((a * b) / _gcd)
 
-# print(lcm(1071, 1029))
-# 52479
-
-# 数が3以上の最大公約数lcm
 def lcm_mlt(a, num_list):
     if len(num_list) == 0:
         return a
@@ -24,6 +17,6 @@ def lcm_mlt(a, num_list):
     return lcm_mlt(lcm(a, b), num_list)
 
 n = int(input())
-nl = list(set([int(input()) for _ in range(n)]))
+nl = [int(input()) for _ in range(n)]
 
 print(lcm_mlt(1, nl))
