9ee977e253134f6196d6962f83e076c4a17966f3
tanin <tanin@ikyu.com>
2020-05-26 22:07:54 +0900

    これなら通った
    型のキャストが何らかの悪影響を及ぼしたのかもしれない

--- abc070c.py
+++ abc070c.py
@@ -1,8 +1,8 @@
-from math import gcd
+from fractions import gcd
 # def lcm(a,b): return a*b//gcd(a,b)
 
-def lcm(a,b):
-    return a*b//gcd(a, b)
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
 
 n = int(input())
 
