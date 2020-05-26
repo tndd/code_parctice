8ab7d1c72dd3359ab0b795c69bbf838bf864a7d4
tanin <tanin@ikyu.com>
2020-05-26 21:50:49 +0900

    ライブラリのlcmを使うように

--- abc070c.py
+++ abc070c.py
@@ -1,20 +1,10 @@
-def gcd(a, b):
-    md = a % b
-    if md == 0:
-        return b
-    return gcd(b, md)
-
-def lcm(a, b):
-    _gcd = gcd(a, b)
-    return int((a * b) / _gcd)
+from fractions import gcd
+def lcm(a,b): return a*b//gcd(a,b)
 
 n = int(input())
 
 ans = 1
 for i in range(n):
     t = int(input())
-    if t >= ans:
-        ans = lcm(t, ans)
-    else:
-        ans = lcm(ans, t)
+    ans = lcm(t, ans)
 print(ans)
