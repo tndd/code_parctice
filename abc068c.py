696e17ff64ef7c883e9f97e518fdc711ff20e588
tanin <tanin@ikyu.com>
2020-05-28 19:34:27 +0900

    上手く行ってそうだけどTLE

--- abc068c.py
+++ abc068c.py
@@ -13,9 +13,9 @@ for _ in range(M):
 # 到達可能島リストに変化があったか？
 is_updated = True
 # 到達可能島リスト
-reachable_islands = [False for _ in range(N)]
+reachable_islands = [float('inf') for _ in range(N)]
 # 初期位置は到達可能なのでTrue
-reachable_islands[0] = True
+reachable_islands[0] = 0
 
 # 新たな島に移動できたなら次も探索する
 while is_updated:
@@ -23,22 +23,19 @@ while is_updated:
     # 各到達可能な島ごとに移動シミュレーション
     for island_num in range(N):
         # 到達し得ない島ならシミュレーションをスキップ
-        if reachable_islands[island_num] == False:
+        if reachable_islands[island_num] == float('inf'):
             continue
         # 全ての船についてシミュレート
         for ship in ships:
             # 現在地の島発の船かつ目標島が到達済みでなければ移動シミュレート実行
-            if island_num == ship.land_from and reachable_islands[ship.land_to] == False:
+            if island_num == ship.land_from and reachable_islands[ship.land_to] == float('inf'):
                 # 到達済の印をつける
-                reachable_islands[ship.land_to] = True
+                reachable_islands[ship.land_to] = reachable_islands[ship.land_from] + 1
                 # 島が更新されたフラグを立てる
                 is_updated = True
 
 # 最後の島が到達可能であるかを判定
-if reachable_islands[N - 1]:
+if reachable_islands[N - 1] <= 2:
     print('POSSIBLE')
 else:
     print('IMPOSSIBLE')
-
-for bl in reachable_islands:
-    print(bl)
