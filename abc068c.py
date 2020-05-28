77d693c86ec2329896b9857f5274fe7afd83f695
tanin <tanin@ikyu.com>
2020-05-28 23:03:36 +0900

    キューを使ったループに変更

--- abc068c.py
+++ abc068c.py
@@ -10,29 +10,28 @@ for _ in range(M):
   f, t = map(int, input().split())
   ships.append(Ship(f, t))
 
-# 到達可能島リストに変化があったか？
-is_updated = True
 # 到達可能島リスト
 reachable_islands = [float('inf') for _ in range(N)]
 # 初期位置は到達可能なのでTrue
 reachable_islands[0] = 0
+# 探索予定島
+search_list = [0]
 
 # 新たな島に移動できたなら次も探索する
-while is_updated:
-  is_updated = False
+while len(search_list):
+  now_island = search_list.pop(0)
   # 各到達可能な島ごとに移動シミュレーション
-  for island_num in range(N):
-    # 到達し得ない島ならシミュレーションをスキップ
-    if reachable_islands[island_num] == float('inf'):
+  if reachable_islands[now_island] == float('inf'):
       continue
-    # 全ての船についてシミュレート
-    for ship in ships:
-      # 現在地の島発の船かつ目標島が到達済みでなければ移動シミュレート実行
-      if island_num == ship.land_from and reachable_islands[ship.land_to] == float('inf'):
-        # 到達済の印をつける
-        reachable_islands[ship.land_to] = reachable_islands[ship.land_from] + 1
-        # 島が更新されたフラグを立てる
-        is_updated = True
+  # 全ての船についてシミュレート
+  for ship in ships:
+    # 現在地の島発の船かつ目標島が到達済みでなければ移動シミュレート実行
+    if now_island == ship.land_from \
+       and reachable_islands[ship.land_to] > reachable_islands[ship.land_from] + 1:
+      # 到達済の印をつける
+      reachable_islands[ship.land_to] = reachable_islands[ship.land_from] + 1
+      # 到着島を探索予定リストに追加
+      search_list.append(ship.land_to)
 
 # 最後の島が到達可能であるかを判定
 if reachable_islands[N - 1] <= 2:
