aeaa70a86bd781d999f7bbf146c5bff00f1f9b50
tanin <tanin@ikyu.com>
2020-05-30 00:43:21 +0900

    二重ループを起こさない合計数の差分の求め方
    forループ内で合計sumを求めると
    二重ループが発生するのでダメになる。
    
    なぜ二重ループになるかというと、
    sum（合計）を求めるためには
    対象のリストの中身をループさせて
    中身を合計する必要があるから。

--- abc067c.py
+++ abc067c.py
@@ -2,10 +2,11 @@ N = int(input())
 a = list(map(int, input().split()))
 
 min_diff = float('inf')
+a_sum = sum(a)
+l_sum = 0
 
-for i in range(1, N):
-  l_sum = sum(a[:i])
-  r_sum = sum(a[i:])
-  min_diff = min(min_diff, abs(l_sum - r_sum))
+for i in range(N - 1):
+  l_sum += a[i]
+  min_diff = min(min_diff, abs(l_sum * 2 - a_sum))
 
 print(min_diff)
