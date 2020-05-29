6200529aace9e61a9aae55689f64a3050e3de1e6
tanin <tanin@ikyu.com>
2020-05-29 20:55:09 +0900

    二回の探索に特化

--- abc068c.py
+++ abc068c.py
@@ -5,36 +5,20 @@ class Ship:
 
 N, M = map(int, input().split())
 
+mid_from = set()
+mid_to = set()
+
 ships = []
 for _ in range(M):
   f, t = map(int, input().split())
-  ships.append(Ship(f, t))
-
-# 到達可能島リスト
-reachable_islands = [float('inf') for _ in range(N)]
-# 初期位置は到達可能なのでTrue
-reachable_islands[0] = 0
-# 探索予定島
-search_list = [0]
-
-# 新たな島に移動できたなら次も探索する
-while len(search_list):
-  now_island = search_list.pop(0)
-  # 各到達可能な島ごとに移動シミュレーション
-  if reachable_islands[now_island] == float('inf'):
-      continue
-  # 全ての船についてシミュレート
-  for ship in ships:
-    # 現在地の島発の船かつ目標島が到達済みでなければ移動シミュレート実行
-    if now_island == ship.land_from \
-       and reachable_islands[ship.land_to] > reachable_islands[ship.land_from] + 1:
-      # 到達済の印をつける
-      reachable_islands[ship.land_to] = reachable_islands[ship.land_from] + 1
-      # 到着島を探索予定リストに追加
-      search_list.append(ship.land_to)
+  ship = Ship(f, t)
+  if ship.land_from == 0:
+    mid_from.add(ship.land_to)
+  if ship.land_to == N - 1:
+    mid_to.add(ship.land_from)
 
 # 最後の島が到達可能であるかを判定
-if reachable_islands[N - 1] == 2:
+if mid_from & mid_to:
   print('POSSIBLE')
 else:
   print('IMPOSSIBLE')
