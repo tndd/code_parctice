be82a9efedd7e65288818b07d4f53ba9af2342be
tanin <tanin@ikyu.com>
2020-05-30 19:14:45 +0900

    期待通りの動作ではない
    理論上は上手くいくと思うのだが

--- abc074c.py
+++ abc074c.py
@@ -3,22 +3,24 @@ def concentration(water, suger):
 
 WATER_A, WATER_B, SUGER_C, SUGER_D, MELT_AMOUNT, LIMIT = map(int, input().split())
 
+WATER_A *= 100
+WATER_B *= 100
+
 max_concentration = 0
 ans_suger_water = 0
 ans_suger = 0
 
-for wa in range(WATER_A + 1):
-  for wb in range(WATER_B + 1):
-    for sc in range(SUGER_C + 1):
-      for sd in range(SUGER_D + 1):
-        water = (wa + wb) * 100
-        suger = sc + sd
+for wa in range(31):
+  for wb in range(31):
+    for sc in range(31):
+      for sd in range(31):
+        water = WATER_A * wa + WATER_B * wb
+        suger = SUGER_C * sc + SUGER_D * sd
         if 0 < water + suger <= LIMIT:
           percent = concentration(water, suger)
           if max_concentration < percent <= MELT_AMOUNT:
-            print(percent)
             max_concentration = percent
             ans_suger_water = water + suger
             ans_suger = suger
 
-print(ans_suger_water, suger)
+print(ans_suger_water, ans_suger)
