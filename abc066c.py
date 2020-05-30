30033d9134eb6206caca2a11f93760ba614012eb
tanin <tanin@ikyu.com>
2020-05-30 18:31:01 +0900

    数列を偶数番と奇数番で分ける部分を修正
    偶数番・奇数番でなく、要素自体が偶数か奇数かという判定になっていた。

--- abc066c.py
+++ abc066c.py
@@ -1,11 +1,11 @@
 n = int(input())
 a = list(map(int, input().split()))
 
-a_even = list(filter(lambda x: x % 2 == 0, a))
-a_odd = list(filter(lambda x: x % 2 != 0, a))
+a_even = [elm_a for i, elm_a in enumerate(a) if i % 2 == 0]
+a_odd = [elm_a for i, elm_a in enumerate(a) if i % 2 == 1]
 
 ans = a_even[::-1] + a_odd
-if n % 2 != 0:
+if n % 2 == 0:
   ans = ans[::-1]
 
 print(' '.join(map(str, ans)))
