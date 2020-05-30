4b9dfee48bb4685b5153be06c78725c11362e323
tanin <tanin@ikyu.com>
2020-05-30 21:47:33 +0900

    見た目の調整

--- abc074c.py
+++ abc074c.py
@@ -5,11 +5,8 @@ WATER_A, WATER_B, SUGER_C, SUGER_D, MELT_PER_100, LIMIT = map(int, input().split
 
 WATER_A *= 100
 WATER_B *= 100
-MELT_PERCENT = concentration(100, MELT_PER_100)
-
-max_concentration = 0
-ans_suger_water = 0
-ans_suger = 0
+# 砂糖の溶解率の上限
+MELT_PERCENT_LIMIT = concentration(100, MELT_PER_100)
 
 wa = wb = sc = sd = 0
 
@@ -35,8 +32,9 @@ while sc * SUGER_C <= LIMIT:
 
 ans_suger_water = 0
 ans_suger = 0
-
+# 最大溶解率を保持するための変数
 max_concentration = 0
+
 for water in water_set:
   for suger in suger_set:
     # 砂糖水の量が0より大きく制限量以下以外の場合は次のループへ
@@ -45,7 +43,7 @@ for water in water_set:
     # 砂糖濃度計算
     suger_precent = concentration(water, suger)
     # 砂糖濃度が最高を更新かつ限界濃度を超えていなければ答えを更新
-    if max_concentration < suger_precent <= MELT_PERCENT:
+    if max_concentration < suger_precent <= MELT_PERCENT_LIMIT:
       max_concentration = suger_precent
       ans_suger_water = water + suger
       ans_suger = suger
