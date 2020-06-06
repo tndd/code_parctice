a25a932af10127e482c21eff72726ecb6b235f50
tan <ttru702@gmail.com>
2020-06-06 21:01:41 +0900

    WA add abc063c

---
+++ @abc063c.py
@@ -0,0 +1,15 @@
+N = int(input())
+S = sorted([int(input()) for _ in range(N)])
+
+ans = sum(S)
+
+if ans % 10 == 0:
+  # find numbers that are not multiples of 10 in order of decreasing.
+  for s in S:
+    if s % 10 != 0:
+      ans -= s
+      break
+  # if all the numbers in the list are multiples of 10, the ans is 0.
+  ans = 0
+
+print(ans)
