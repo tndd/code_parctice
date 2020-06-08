ae60e1b9db9de4a47a633b6082575fb61348a0df
tan <ttru702@gmail.com>
2020-06-08 23:22:28 +0900

    WA abc059c
    it moves away from the answer.

--- @abc059c.py
+++ @abc059c.py
@@ -4,20 +4,23 @@ A = list(map(int, input().split()))
 ans = 0
 prev_sm = A[0] # total to i - 1
 for i in range(1, N):
-  # if prev_sum is plus and a is more minus than prev_sum.
+  # if prev_sum is plus and A[i] is more minus than prev_sum.
   if prev_sm > 0 and prev_sm + A[i] < 0:
-    pass
-  # if prev_sum is plus and a is larger than or equal to prev_sum.
+    prev_sm += A[i]
+  # if prev_sum is plus and A[i] is larger than or equal to prev_sum.
   elif prev_sm > 0 and prev_sm + A[i] >= 0:
-    ans += prev_sm + A[i] + 1
-    A[i] -= prev_sm + A[i] + 1
-  # if prev_sum is minus and a is more plus than prev_sum.
+    diff = abs(prev_sm + A[i] + 1)
+    prev_sm += A[i]
+    ans += diff
+    A[i] -= diff
+  # if prev_sum is minus and A[i] is more plus than prev_sum.
   elif prev_sm < 0 and prev_sm + A[i] > 0:
-    pass
-  # if prev_sum is minus and a is more smaller than or equal to prev_sum.
+    prev_sm += A[i]
+  # if prev_sum is minus and A[i] is more smaller than or equal to prev_sum.
   elif prev_sm < 0 and prev_sm + A[i] <= 0:
-    ans += -(prev_sm + A[i] - 1)
-    A[i] += -(prev_sm + A[i] + 1)
-  prev_sm += A[i]
+    diff = abs(prev_sm + A[i] - 1)
+    prev_sm += A[i]
+    ans += diff
+    A[i] += diff
 
 print(ans)
