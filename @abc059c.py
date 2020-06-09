91e554cc060c104adf2ba873e2cf913feff87ebf
tan <ttru702@gmail.com>
2020-06-09 22:29:03 +0900

    WIP

--- @abc059c.py
+++ @abc059c.py
@@ -3,39 +3,28 @@ A = list(map(int, input().split()))
 
 ans = 0
 previous_sum = A[0] # total to i - 1
+now_sum = previous_sum
 for i in range(1, N):
-  # if prev_sum is plus and A[i] is more minus than prev_sum, no action.
-  # if prev_sum is plus and minus of A[i] is larger than or equal to prev_sum.
-  if previous_sum > 0 and previous_sum + A[i] >= 0:
+  now_sum += A[i]
+  # if previous_sum is plus and now_sum is more minus than previous_sum, no action.
+  # if previous_sum is plus and minus of now_sum is smaller than or equal to previous_sum.
+  if previous_sum > 0 and previous_sum + now_sum >= 0:
     # the number that need to be subtracted to meet the requirements.
-    require_subtraction = previous_sum + A[i] + 1
-    # the number of wastes in meeting the requirement.
-    distance_from_ideal = previous_sum - 1
-    # restration to an ideal list.
-    A[i - 1] -= distance_from_ideal
-    previous_sum -= distance_from_ideal
-    ans += distance_from_ideal
-    # adjustment for the manupilation of the previous values.
-    require_subtraction -= distance_from_ideal
+    require_subtraction = previous_sum - now_sum + 1
     # correcting the current number.
     A[i] -= require_subtraction
+    now_sum -= require_subtraction
     ans += require_subtraction
-  # if prev_sum is minus and A[i] is more plus than prev_sum, no action.
-  # if prev_sum is minus and minus of A[i] is more smaller than or equal to prev_sum.
-  elif previous_sum < 0 and previous_sum + A[i] <= 0:
+  # if previous_sum is minus and now_sum is more plus than previous_sum, no action.
+  # if previous_sum is minus and plus of now_sum is more smaller than or equal to previous_sum.
+  elif previous_sum < 0 and previous_sum + now_sum <= 0:
     # the number that need to be added to meet the requirements.
-    require_addition = -(previous_sum + A[i] - 1)
-    # the number of wastes in meeting the requirement.
-    distance_from_ideal = -1 - previous_sum 
-    # restration to an ideal list.
-    A[i - 1] += distance_from_ideal
-    previous_sum += distance_from_ideal
-    ans += distance_from_ideal
-    # adjustment for the manupilation of the previous values.
-    require_addition -= distance_from_ideal
+    require_addition = -(previous_sum - now_sum - 1)
     # correcting the current number.
     A[i] += require_addition
+    now_sum += require_addition
     ans += require_addition
   previous_sum += A[i]
 
+print(A)
 print(ans)
