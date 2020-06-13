e97af5b0462ab0f0bb0d81b2580979f1f0464cab
tan <ttru702@gmail.com>
2020-06-13 23:31:04 +0900

    wa, limit loop

--- tokiomarine2020c.py
+++ tokiomarine2020c.py
@@ -1,12 +1,13 @@
 N, K = list(map(int, input().split()))
 A = list(map(int, input().split()))
+empty = [0 for _ in range(N)]
 
-for k in range(K):
-  plus = [0 for _ in range(N)]
+for k in range(min(K, 1000)):
+  plus = empty[:]
   for n in range(N):
     l = n - A[n]
     r = n + A[n]
-    for i in range(l, r + 1):
+    for i in range(l, r + 1): 
       if 0 <= i < N:
         plus[i] += 1
   A = plus
