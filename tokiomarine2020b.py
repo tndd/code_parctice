aaf433707c4a3feb63a7e8c991c2bafe6bc08473
tan <ttru702@gmail.com>
2020-06-13 22:20:49 +0900

    wa, why?

--- tokiomarine2020b.py
+++ tokiomarine2020b.py
@@ -2,31 +2,10 @@ oni, oni_spd = list(map(int, input().split()))
 child, child_spd = list(map(int, input().split()))
 T = int(input())
 
-# 4 dist_from_oni(+)
-# -1 ofs_spd(-)
-# T = 5
-# expect false
-# -5 >= 4
+dist = abs(child - oni)
+ofs_spd = oni_spd - child_spd
 
-# -4
-# 1
-# T = 5
-# expect false
-# 5 <= -4
-
-dist_from_oni = child - oni     # -4
-ofs_spd = oni_spd - child_spd   # -1
-# T = 5
-# -5 >= -4
-if dist_from_oni >= 0 and ofs_spd >= 0:
-  if ofs_spd * T >= dist_from_oni:
+if ofs_spd * T >= dist and dist / ofs_spd % 2 == 0 and 0 <= dist / ofs_spd <= T:
     print('YES')
-  else:
-    print('NO')
-elif dist_from_oni <= 0 and ofs_spd <= 0:
-  if ofs_spd * T <= dist_from_oni:
-    print('YES')
-  else:
-    print('NO')
 else:
   print('NO')
