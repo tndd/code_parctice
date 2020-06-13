2620362360b192bb1f7f5858b07eb56fc3445e34
tan <ttru702@gmail.com>
2020-06-13 22:25:51 +0900

    AC tokiomarine2020b

--- tokiomarine2020b.py
+++ tokiomarine2020b.py
@@ -5,7 +5,7 @@ T = int(input())
 dist = abs(child - oni)
 ofs_spd = oni_spd - child_spd
 
-if ofs_spd * T >= dist and dist / ofs_spd % 2 == 0 and 0 <= dist / ofs_spd <= T:
-    print('YES')
+if ofs_spd * T >= dist:
+  print('YES')
 else:
   print('NO')
