fc99fa97caea09392ab32095ad400f871b5e03df
tan <ttru702@gmail.com>
2020-06-15 17:25:04 +0900

    auto test
    it's not working in all samples.

--- problems/abc057_c.py
+++ problems/abc057_c.py
@@ -1,14 +1,14 @@
 from math import sqrt
-
+ 
 N = int(input())
-
+ 
 ans = float('inf')
-
+ 
 for a in range(1, int(sqrt(N)) + 1):
   if N % a == 0:
     A = str(a)
     B = str(N // a)
     F = max(len(A), len(B))
     ans = min(ans, F)
-
+ 
 print(ans)
