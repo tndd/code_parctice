8ea62e63eb54153bce7046f307717ef7a87e452b
tan <ttru702@gmail.com>
2020-06-26 23:35:30 +0900

    AC abc170d
    avoid TLE.

--- problems/abc170_d.py
+++ problems/abc170_d.py
@@ -4,19 +4,20 @@ THE_NUM = 10**6 + 1
 
 dp = [True] * THE_NUM
 
+prev_num = -1
 for i in A:
-  for j in range(i * 2, THE_NUM, i):
-    dp[j] = False
+  # if the same number appears as before, that number cannot be counted in the answer.
+  if prev_num != i:
+    for j in range(i * 2, THE_NUM, i):
+      dp[j] = False
+  else:
+    dp[i] = False
+  prev_num = i
 
 ans = 0
-prev_num = -1
-for i, x in enumerate(A):
+for i in A:
   # the same number after the second will not counted in the answer.
-  if dp[x] == True and prev_num != i:
+  if dp[i] == True:
     ans += 1
-
-# if they are all the same number, the answer is 0.
-if A[0] == A[-1]:
-  ans = 0
 
 print(ans)
