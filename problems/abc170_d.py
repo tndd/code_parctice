25bd0f6d703ff43f3266ba6a0b236c44bfa20674
tan <ttru702@gmail.com>
2020-06-26 23:17:00 +0900

    WA
    It doesn't take into account the same number.

--- problems/abc170_d.py
+++ problems/abc170_d.py
@@ -2,34 +2,12 @@ N = int(input())
 A = sorted(list(map(int, input().split())))
 THE_NUM = 10**6 + 1
 
-dp = [True] * (THE_NUM)
+dp = [True] * THE_NUM
 
-for i in range(2, THE_NUM):
+for i in A:
   for j in range(i * 2, THE_NUM, i):
     dp[j] = False
 
-print(dp)
+anss = [i + 1 for i, x in enumerate(A) if dp[x] == True]
 
-# anss = [i + 1 for i, x in enumerate(A[2:]) if dp[x] == False]
-
-# print(anss)
-
-# ans = 0
-# for i in range(N):
-#   is_divide = False
-#   for j in range(i):
-#     if A[i] % A[j] == 0:
-#       is_divide = True
-#       break
-#   if is_divide == False:
-#     ans += 1
-
-# # same number pattern.
-# if A[0] in A[1:]:
-#   ans -= 1
-
-# # single number pattern.
-# if N == 1:
-#   ans -= 1
-
-# print(ans)
+print(len(anss))
