9afe73fd3584969189d4f1ca10b6a61e28292579
tan <ttru702@gmail.com>
2020-06-26 23:22:39 +0900

    WA why ?

--- problems/abc170_d.py
+++ problems/abc170_d.py
@@ -8,6 +8,15 @@ for i in A:
   for j in range(i * 2, THE_NUM, i):
     dp[j] = False
 
-anss = [i + 1 for i, x in enumerate(A) if dp[x] == True]
+ans = 0
+prev_num = -1
+for i, x in enumerate(A):
+  # the same number after the second will not counted in the answer.
+  if dp[x] == True and prev_num != i:
+    ans += 1
 
-print(len(anss))
+# if they are all the same number, the answer is 0.
+if A[0] == A[-1]:
+  ans = 0
+
+print(ans)
