7bac3f48ec23a56b73e35b1bf26b359ea1e11bde
tan <ttru702@gmail.com>
2020-06-27 22:18:17 +0900

    wa single list

--- problems/abc172_c.py
+++ problems/abc172_c.py
@@ -2,28 +2,14 @@ N, M, K = list(map(int, input().split()))
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
+L = sorted(A + B)
+
 time = 0
 ans = 0
-while True:
-  al = len(A)
-  bl = len(B)
-  if al > 0 and bl > 0 and (time + A[0] <= K or time + B[0] <= K):
-    pass
-  elif al > 0 and bl == 0 and (time + A[0] <= K):
-    pass
-  elif al == 0 and bl > 0 and (time + B[0] <= K):
-    pass
+for x in L:
+  if time + x <= K:
+    ans += 1
+    time += x
   else:
     break
-  if al > 0 and bl > 0:
-    if A[0] >= B[0]:
-      time += B.pop(0)
-    else:
-      time += A.pop(0)
-  elif al > 0 and bl == 0:
-    time += A.pop(0)
-  elif al == 0 and bl > 0:
-    time += B.pop(0)
-  ans += 1
-
 print(ans)
